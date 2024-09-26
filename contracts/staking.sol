d6491c43d99e0481b9f5e4961e75e94fa9395f01
David Drobnak <david.drobnak26@gmail.com>
2024-09-27 00:10:51 +0200

    feat: basic staking contract

---
+++ contracts/staking.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/*
+    TODO: Users should be
+        eligible for rewards only when they
+        stake `delay` before `vestingStart`
+*/
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StakingLinearVesting {
+    // tokens
+    IERC20 stakingToken;
+    IERC20 rewardToken;
+
+    // accounting
+    mapping(address user => uint256 amount) public stakedAmount;
+    uint256 public totalStaked;
+
+    mapping(address user => uint256 amount) public rewardsReceived;
+    uint256 public totalRewards;
+
+    // vesting
+    uint256 vestingStart;
+    uint256 vestingDuration;
+    uint256 delay;
+
+    constructor(address _stakingToken, address _rewardToken, uint256 _vestingStart, uint256 _vestingDuration, uint256 _delay) {
+        stakingToken = IERC20(stakingToken);
+        rewardToken = IERC20(rewardToken);
+
+        vestingStart = _vestingStart;
+        vestingDuration = _vestingDuration;
+        delay = _delay;
+    }
+
+    function pendingRewards(address _user) public view returns (uint256) {
+        require(vestingStart >= block.timestamp);
+
+        uint256 stakedAmount = stakedAmount[_user];
+        uint256 rewardsReceived = rewardsReceived[_user];
+        uint256 rewardPerToken = (totalRewards * (block.timestamp - vestingStart) / vestingDuration) / totalStaked;
+
+        return (stakedAmount * rewardPerToken) - rewardsReceived;
+    }
+
+    function stake(uint256 _amount) external {
+        require(_amount > 0);
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        stakedAmount[msg.sender] += _amount;
+        totalStaked += _amount;
+    }
+
+    // TODO: add receiver argument
+    function claimRewards() external {
+        uint256 rewards = pendingRewards(msg.sender);
+
+        rewardsReceived[msg.sender] += rewards;
+
+        rewardToken.transferFrom(address(this), msg.sender, rewards);
+    }
+
+    function addRewards(uint256 _amount) external {
+        require(_amount > 0);
+        rewardToken.transferFrom(msg.sender, address(this), _amount);
+
+        totalRewards += _amount;
+    }
+}
